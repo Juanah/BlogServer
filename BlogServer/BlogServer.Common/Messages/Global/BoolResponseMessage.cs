0ca07f205356660f370d930897ef54943a8923d9
jonas ahlf <jonas@juanah.de>
2015-02-15 06:26:58 +0100

    First MessageProcessor for UserMessages

---
+++ BlogServer/BlogServer.Common/Messages/Global/BoolResponseMessage.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace BlogServer.Common
+{
+	public class BoolResponseMessage
+	{
+		public bool Success{ get; set;}
+	}
+}
+
---
+++ BlogServer/BlogServer.Processor/MessageProcessor/UserMessageProcessor.cs
@@ -0,0 +1,66 @@
+using System;
+using BlogServer.DataAccess;
+using BlogServer.Common;
+using System.Linq;
+using Newtonsoft.Json;
+using log4net;
+namespace BlogServer.Processor
+{
+	/// <summary>
+	/// Handels the Incomming and outgoing UserMessages
+	/// </summary>
+	public class UserMessageProcessor
+	{
+		private UserRepository _userRepo;
+		private ILog _log = LogManager.GetLogger(typeof(UserMessageProcessor))]
+		public UserMessageProcessor (UserRepository _userRepo)
+		{
+			this._userRepo = _userRepo;
+		}
+
+		/// <summary>
+		/// Tries to add the User and give back the result
+		/// </summary>
+		/// <returns>The user.</returns>
+		/// <param name="message">Message.</param>
+		public BoolResponseMessage AddUser(string message)
+		{
+			_log.Debug (String.Format ("Got UserAddmessage: {0} ", message));
+
+			//Default message is always false
+			var response = new BoolResponseMessage () {
+				Success = false
+			};
+
+			UserAdd addMessage = null; 
+			try {
+				 addMessage = JsonConvert.DeserializeObject<UserAdd>(message);
+			} catch (Exception ex) {
+				_log.Warn (String.Format ("Bad JsonFormat from Message {0} ", message));
+				return response;
+			}
+
+			if (addMessage == null) {
+				return response;
+			}
+
+			if (!_userRepo.User.Any (n => n.Username.Equals(""))) {
+				//TODO das geht besser ;)
+				BlogImage emptyImage = new BlogImage () {
+					Path = ""
+				};
+				User newUser = new User () {
+					Username = addMessage.ToAdd.Username,
+					Authentification = addMessage.ToAdd.Authentification,
+					RSAPublicKey = "",
+					Image = emptyImage
+				};
+				_userRepo.AddUser (newUser);
+				response.Success = true;
+				return response;
+			}
+			return response;
+		}
+	}
+}
+
